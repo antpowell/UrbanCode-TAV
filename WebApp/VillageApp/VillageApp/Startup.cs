63500c7206e97b4283b2bbe49d959f85446fb51e
dotsonus <dotsonus.tsu@gmail.com>
2017-02-17 23:35:46 -0600

    Added VS MVC project WebApp

---
+++ WebApp/VillageApp/VillageApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VillageApp.Startup))]
+namespace VillageApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
